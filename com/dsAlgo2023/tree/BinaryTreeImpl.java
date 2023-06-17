8ad7e93b54dd80d27b5d80760e61e9e7b1fa5d22
ishwar chaiser <ishwarchaiser@gmail.com>
2023-06-17 10:06:06 +0530

    Binary tree inOrder traversal

--- com/dsAlgo2023/tree/BinaryTreeImpl.java
+++ com/dsAlgo2023/tree/BinaryTreeImpl.java
@@ -12,36 +12,59 @@ public class BinaryTreeImpl {
         myTree.add(7);
         myTree.add(9);
         System.out.println(myTree.root);
+        System.out.println("-------print in level--------");
         myTree.traverseInLevel();
+        System.out.println("\n-------print in order--------");
+        myTree.traverseInOrder();
+
+        myTree=new BinaryTree();
+        myTree.root=new TreeNode(1);
+        myTree.root.left=new TreeNode(2);
+        myTree.root.right=new TreeNode(3);
+        myTree.root.left.left=new TreeNode(4);
+        myTree.root.left.left.left=new TreeNode(5);
+        myTree.root.left.left.right=new TreeNode(6);
+        myTree.root.left.left.right.left=new TreeNode(7);
+        myTree.root.left.left.right.right=new TreeNode(8);
+        myTree.root.left.left.right.right.left=new TreeNode(9);
+        myTree.root.right=new TreeNode(3);
+        myTree.root.right.left=new TreeNode(10);
+        myTree.root.right.right=new TreeNode(12);
+        myTree.root.right.left=new TreeNode(10);
+        myTree.root.right.left.left=new TreeNode(11);
+        myTree.root.right.left.left.right=new TreeNode(13);
+        System.out.println("\n-------print complex binary tree in order--------");
+        myTree.traverseInOrder();
     }
 
+}
+
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){};
+    TreeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "data=" + data +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
 }
 
 class BinaryTree{
     TreeNode root;
 
 
-    class TreeNode{
-        int data;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(){};
-        TreeNode(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
-        }
-
-        @Override
-        public String toString() {
-            return "TreeNode{" +
-                    "data=" + data +
-                    ", left=" + left +
-                    ", right=" + right +
-                    '}';
-        }
-    }
 
     public void add(int data){
         root=insertTree(root,data);
@@ -72,6 +95,18 @@ class BinaryTree{
             System.out.print(levelQ.peek().data+"   ");
             levelQ.poll();
         }
+    }
 
+    public void traverseInOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(TreeNode root){
+        if (root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+"   ");
+        inOrder(root.right);
     }
 }
