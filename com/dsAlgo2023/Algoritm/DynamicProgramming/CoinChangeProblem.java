01e5e4e2f3f7e569b4db2e63e864885f4e62f7b5
ishwar chaiser <ishwarchaiser@gmail.com>
2023-07-20 08:08:07 +0530

    Dynamic Programming : Coin Change problem with dp

--- com/dsAlgo2023/Algoritm/DynamicProgramming/CoinChangeProblem.java
+++ com/dsAlgo2023/Algoritm/DynamicProgramming/CoinChangeProblem.java
@@ -6,7 +6,7 @@ public class CoinChangeProblem {
     public static void main(String[] args) {
         int [] coinTypes = new int[]{7,5,1};
         int sum=18;
-        Integer dp[]=new Integer[sum+1];
+        Integer[] dp =new Integer[sum+1];
         Arrays.fill(dp,-1);
 
         System.out.println("minimum coins: "+minimumCoins(coinTypes,sum,dp));
@@ -25,12 +25,10 @@ public class CoinChangeProblem {
         int minimumCoins = Integer.MAX_VALUE;
         int result=0;
         for(int coin : coinTypes){
-            if((sum-coin)>=0 && dp[sum-coin]!=-1){
+            if((sum-coin)>=0 && dp[sum-coin]!=-1)
                 result = dp[sum-coin];
-            }
-            else{
+            else
                 result = minimumCoins(coinTypes,sum-coin, dp);
-            }
 
             if(result!=Integer.MAX_VALUE)
                 minimumCoins=Integer.min(minimumCoins,result+1 );
