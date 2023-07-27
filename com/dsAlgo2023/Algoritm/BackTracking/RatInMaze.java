db92c7f41e891baa486ca32436b768733541f1b1
ishwar chaiser <ishwarchaiser@gmail.com>
2023-07-27 20:58:59 +0530

    Back Tracking Algorithm : Rat in the Maze for multiple paths

--- com/dsAlgo2023/Algoritm/BackTracking/RatInMaze.java
+++ com/dsAlgo2023/Algoritm/BackTracking/RatInMaze.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class RatInMaze {
     public static void main(String[] args) {
-        int maze[][] =
+        int[][] maze =
                 {{1, 0, 0, 0},
                  {1, 1, 0, 1},
                  {1, 1, 0, 0},
@@ -16,59 +16,41 @@ public class RatInMaze {
     private static ArrayList<String> findPaths(int[][] maze) {
         int [][] sol = new int[maze.length][maze.length];
         ArrayList<String>paths= new ArrayList<>();
-        findPaths(maze,0,0,sol,new StringBuilder(),null,paths);
+        findPaths(maze,0,0,sol,new StringBuilder(),"",paths);
         return paths;
     }
 
-    private static boolean findPaths(int[][] maze, int x, int y, int[][]sol, StringBuilder path, String direction, List<String>paths){
+    private static void findPaths(int[][] maze, int x, int y, int[][]sol, StringBuilder path, String direction, List<String>paths){
         if(x== maze.length-1 && y == maze.length-1 && maze[x][y]==1){
-            System.out.println("* reached "+x+","+y);
             sol[x][y]=1;
             path.append(direction);
-            System.out.println("* reached path: "+path);
             paths.add(path.toString());
-            return true;
+            return ;
         }
-        if(isSafe(maze, x, y, direction, path, sol)){
+        if(isSafe(maze, x, y, sol)){
             sol[x][y]=1;
+            path.append(direction);
             //UP
-            if(findPaths(maze, x-1, y, sol, path, "U", paths)){
-                return true;
-            }
+            findPaths(maze, x-1, y, sol, path, "U", paths);
             //DOWN
-            if(findPaths(maze, x+1, y, sol, path, "D", paths)){
-                return true;
-            }
+            findPaths(maze, x+1, y, sol, path, "D", paths);
             //LEFT
-            if(findPaths(maze, x, y-1, sol, path, "L", paths)){
-                return true;
-            }
+            findPaths(maze, x, y-1, sol, path, "L", paths);
             //RIGHT
-            if(findPaths(maze, x, y+1, sol, path, "R", paths)){
-                return true;
-            }
+            findPaths(maze, x, y+1, sol, path, "R", paths);
             //backtrack
             sol[x][y]=0;
-            System.out.println("#Back tracking \n#Deleting: "+ path.charAt(path.length()-1));
             path.deleteCharAt(path.length()-1);
         }
-        return false;
     }
 
-    private static boolean isSafe(int[][] maze, int x, int y, String direction, StringBuilder path, int [][]sol){
+    private static boolean isSafe(int[][] maze, int x, int y, int [][]sol){
 
         if(x<0 || y<0 || x>(maze.length-1)|| y>(maze.length-1)) return false;
 
         if(sol[x][y]==maze[x][y]) return false;
 
-        if(maze[x][y]==0) return false;
-        System.out.println(direction);
-        System.out.println(x+","+y);
-        if(direction!=null){
-            path.append(direction);
-            System.out.println("path: "+path);
-        }
-        return true;
+        return maze[x][y] != 0;
     }
 }
 
