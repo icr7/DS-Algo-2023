a8d6dc3b27e77fc3ac3b038b583e19848d4e55e3
ishwar chaiser <ishwarchaiser@gmail.com>
2023-08-06 11:19:29 +0530

    cmbination : added dp for optimization

--- com/dsAlgo2023/recursion/CoinCombinationForSum.java
+++ com/dsAlgo2023/recursion/CoinCombinationForSum.java
@@ -1,22 +1,32 @@
 package com.dsAlgo2023.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CoinCombinationForSum {
     public static void main(String[] args) {
         int[] coins = new int[]{1, 2, 3};
-        int sum = 5;
+        int sum = 7;
         System.out.println(coinsCount(sum, coins));
     }
 
     private static int coinsCount(int sum, int[] coins) {
-        return coinsCount(sum, coins, 0);
+        Map<String,Integer> coinMap= new HashMap<>();
+        return coinsCount(sum, coins, 0, coinMap);
     }
 
-    private static int coinsCount(int sum, int[] coins, int index) {
+    private static int coinsCount(int sum, int[] coins, int index, Map<String,Integer>coinMap) {
         if (sum == 0) return 1;
         if (sum < 0) return 0;
         int result = 0;
+        String key=(index+1)+"/"+sum;
+        if(coinMap.containsKey(key)){
+            return coinMap.get(key);
+        }
         for (int i = index; i < coins.length; i++)
-            result += coinsCount(sum - coins[i], coins, i);
+            result += coinsCount(sum - coins[i], coins, i,coinMap);
+
+        coinMap.put(key,result);
 
         return result;
     }
