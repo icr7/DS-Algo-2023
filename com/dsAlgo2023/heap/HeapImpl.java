d16846d2a1bbc7afe12166828285f139679a1839
ishwar chaiser <ishwarchaiser@gmail.com>
2023-09-03 15:53:21 +0530

    Heap: insert and delete in heap

--- com/dsAlgo2023/heap/HeapImpl.java
+++ com/dsAlgo2023/heap/HeapImpl.java
@@ -6,13 +6,23 @@ public class HeapImpl {
         for(int i=0;i<=10;i++)
             heap[i]=i;
         int n = 10;
-
+        printHeap(heap,n);
+        System.out.println("\nAfter converting array into heap ");
         buildHeap(heap,n);
+        printHeap(heap,n);
+        int index = 3; //i.e 7
+        System.out.println("\nAfter deleting "+heap[index]);
+        n = deleteFromHeap(index, heap, n);
+        printHeap(heap,n);
+        int data = 7;
+        System.out.println("\nAfter inserting 7 into Heap ");
+        n=insertIntoHeap(data,heap,n);
         printHeap(heap,n);
 
     }
 
-    public static void buildHeap(int[] heap, int n){
+
+    private static void buildHeap(int[] heap, int n){
         int lastParentNode = ( n - 1) / 2;
         for(int i = lastParentNode; i>=0; i--)
             heapify(heap,i,n);
@@ -41,6 +51,27 @@ public class HeapImpl {
         int i=0;
         while (i!=n)
             System.out.print(heap[i++]+" ");
+    }
+
+    private static int deleteFromHeap(int index, int[] heap, int n) {
+        heap[index]=heap[--n];
+        heapify(heap,index,n);
+        return n;
+    }
+
+    private static int insertIntoHeap(int data, int[] heap, int n) {
+        heap[n++]=data;
+        int i = n-1;
+        while(i>0){
+            int parent = (i-1)/2;
+            if(heap[parent]<heap[i]){
+                int temp = heap[parent];
+                heap[parent]=heap[i];
+                heap[i]=temp;
+            }
+            break;
+        }
+        return n;
     }
 
 }
