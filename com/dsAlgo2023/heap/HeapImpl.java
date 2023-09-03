459ee47f9f36be8c017e51676ec3037a657c30a4
ishwar chaiser <ishwarchaiser@gmail.com>
2023-09-03 14:56:41 +0530

    Heap: convert array into heap

--- com/dsAlgo2023/heap/HeapImpl.java
+++ com/dsAlgo2023/heap/HeapImpl.java
@@ -2,76 +2,45 @@ package com.dsAlgo2023.heap;
 
 public class HeapImpl {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
-        buildHeap(arr);
-        printHeap(arr);
-        arr=insertIntoHeap(arr, 16);
-        System.out.println("\nAfter adding 16 into heap: ");
-        printHeap(arr);
-        arr=delete(arr,2);
-        System.out.println("\nAfter deleting arr[2] i.e 16 from heap: ");
-        printHeap(arr);
-    }
+        int [] heap = new int[1000];
+        for(int i=0;i<=10;i++)
+            heap[i]=i;
+        int n = 10;
 
-    public static void buildHeap(int[] arr) {
-        int lastLeafNode = (arr.length / 2) - 1;
-        for (int i = lastLeafNode; i >= 0; i--) {
-            heapify(arr, i);
-        }
+        buildHeap(heap,n);
+        printHeap(heap,n);
 
     }
 
-    public static void heapify(int[] arr, int i) {
+    public static void buildHeap(int[] heap, int n){
+        int lastParentNode = ( n - 1) / 2;
+        for(int i = lastParentNode; i>=0; i--)
+            heapify(heap,i,n);
+    }
+
+    private static void heapify(int[] heap, int i, int n) {
         int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+        int left = 2*i+1;
+        int right = 2*i+2;
 
+        if(left<n && heap[left]>heap[largest])
+            largest=left;
+        if(right<n && heap[right]>heap[largest])
+            largest= right;
 
-        if (left < arr.length && arr[left] > arr[largest]) largest = left;
-        if (right < arr.length && arr[right] > arr[largest]) largest = right;
-        if (largest != i) {
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
-            heapify(arr, largest);
+        if(largest!=i){
+            int temp = heap[i];
+            heap[i] = heap[largest];
+            heap[largest] = temp;
+            heapify(heap,largest,n);
         }
+
     }
 
-    public static void printHeap(int[] heap) {
-        for (int x : heap) {
-            System.out.print(x + " ");
-        }
+    private static void printHeap(int[] heap, int n) {
+        int i=0;
+        while (i!=n)
+            System.out.print(heap[i++]+" ");
     }
 
-    public static int[] insertIntoHeap(int[] heap, int data) {
-        int[] newHeap = new int[heap.length + 1];
-        for (int i = 0; i < heap.length; i++)
-            newHeap[i] = heap[i];
-
-        int currPos = newHeap.length - 1;
-        int parentPos = (currPos - 1) / 2;
-
-        newHeap[currPos] = data;
-
-        if (currPos == 0) return newHeap;
-
-        while (newHeap[currPos] > newHeap[parentPos]) {
-            int temp = newHeap[currPos];
-            newHeap[currPos] = newHeap[parentPos];
-            newHeap[parentPos] = temp;
-            currPos = parentPos;
-            parentPos = (currPos - 1) / 2;
-        }
-        return newHeap;
-    }
-
-    public static int[] delete(int[] heap, int index){
-        if(heap.length==0) return heap;
-        heap[index]=heap[heap.length-1];
-        int[] newHeap = new int[heap.length-1];
-        for(int i=0;i<heap.length-1;i++)
-            newHeap[i]=heap[i];
-        heapify(newHeap,index);
-        return newHeap;
-    }
 }
