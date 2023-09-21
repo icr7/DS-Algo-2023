cc8daf93703a54987ba18d3a372084a79258e0f3
ishwar chaiser <ishwarchaiser@gmail.com>
2023-09-21 08:22:02 +0530

    Sorting: Insertion Sort

---
+++ com/dsAlgo2023/Algoritm/Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package com.dsAlgo2023.Algoritm.Sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = new int[]{7,3,8,9,1};
+        sort(arr);
+        for(int x : arr)
+            System.out.print(x+" ");
+    }
+
+    private static void sort(int[] arr) {
+        for(int i = 1;i< arr.length;i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+
+    }
+}
