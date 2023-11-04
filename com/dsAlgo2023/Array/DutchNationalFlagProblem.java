5c945e671b6917a8e0182e1b46199a55b916b30d
ishwar chaiser <ishwarchaiser@gmail.com>
2023-11-04 21:31:03 +0530

    print 0's 1's and 2's by there position

--- com/dsAlgo2023/Array/DutchNationalFlagProblem.java
+++ com/dsAlgo2023/Array/DutchNationalFlagProblem.java
@@ -2,7 +2,7 @@ package com.dsAlgo2023.Array;
 
 public class DutchNationalFlagProblem {
     public static void main(String[] args) {
-        int [] arr = new int[]{0,2,1,2,0};
+        int [] arr = new int[]{0,2,1,1,0,0,2,1};
         DutchNationalFlagSolution.sort012(arr, arr.length);
         for(int a: arr) System.out.print(a+", ");
     }
@@ -17,6 +17,16 @@ class DutchNationalFlagSolution
             else if(a[i]==1) i++;
             else if(a[i]==2) swap(a,i,k--);
         }
+        System.out.print("0's -> from index 0 to j-1 : ");print(a, 0, j-1);
+        System.out.print("1's -> from index j to k : ");print(a, j, k);
+        System.out.print("2's -> from index i to n-1 : ");print(a, i, n-1);
+
+    }
+
+    private static void print(int[] a, int x, int y) {
+        for (int i=x;i<=y;i++)
+            System.out.print(a[i]+", ");
+        System.out.print("\n");
     }
 
     public static void swap(int[] a, int x, int y){
