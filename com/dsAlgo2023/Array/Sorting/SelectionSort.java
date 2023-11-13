1c56e0c29a4a201a0f55200d997317a26893e9c7
ishwar chaiser <ishwarchaiser@gmail.com>
2023-11-13 14:24:52 +0530

    Selection Sort

--- com/dsAlgo2023/Array/Sorting/SelectionSort.java
+++ com/dsAlgo2023/Array/Sorting/SelectionSort.java
@@ -4,13 +4,13 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{6, 9, 7, 8, 5, 3, 2, 4, 1, 4};
         for (int i = 0; i < arr.length - 1; i++) {
-            int min = i;
-            int temp = arr[i];
+            int min_pos = i;
             for (int j = i + 1; j < arr.length; j++)
-                if (arr[min] > arr[j]) min = j;
+                if (arr[min_pos] > arr[j]) min_pos = j;
 
-            arr[i] = arr[min];
-            arr[min] = temp;
+            int temp = arr[i];
+            arr[i] = arr[min_pos];
+            arr[min_pos] = temp;
         }
         for (int x : arr) System.out.print(x + ", ");
     }
