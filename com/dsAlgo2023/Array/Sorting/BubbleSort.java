404335f45efcfefbbd02ebcb4c4d82578f7b6c5c
ishwar chaiser <ishwarchaiser@gmail.com>
2023-11-14 11:02:51 +0530

    Bubble Sort

--- com/dsAlgo2023/Array/Sorting/BubbleSort.java
+++ com/dsAlgo2023/Array/Sorting/BubbleSort.java
@@ -2,21 +2,16 @@ package com.dsAlgo2023.Array.Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int []arr = new int[]{7,7,6,9,8,1,3,2,4,5,3,1,2};
-
-        for(int i=0;i< arr.length;i++)
-            swap(arr.length-1-i,arr);
-
-        for(int x:arr)
-            System.out.print(x+", ");
+        int[] arr = new int[]{7, 7, 6, 9, 8, 1, 3, 2, 4, 5, 3, 1, 2};
+        int N = arr.length - 1;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N - i; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+        for (int x : arr) System.out.print(x + ", ");
     }
 
-    private static void swap(int n, int[] arr) {
-        for(int i=0;i<n;i++)
-            if(arr[i]>arr[i+1]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-            }
-    }
 }
