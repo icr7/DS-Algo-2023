9f282914f7ef1f9997aa95461ef23294ec9d2596
ishwar chaiser <ishwarchaiser@gmail.com>
2023-11-21 08:31:07 +0530

    Quick Sort

--- com/dsAlgo2023/Array/Sorting/InsertionSort.java
+++ com/dsAlgo2023/Array/Sorting/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
         int[] arr = new int[]{7, 3, 8, 9, 1};
 
         for (int i = 1; i < arr.length; i++)
-            for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+            for (int j = i;( j > 0 && arr[j - 1] > arr[j]); j--) {
                 int temp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
