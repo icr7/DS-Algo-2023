1f03f38523f56b86043fcf9ead10ae9e407d9f22
ishwar chaiser <ishwarchaiser@gmail.com>
2023-12-17 13:09:32 +0530

    Linked List: Deletion at position

--- com/dsAlgo2023/linkedList/LinkedList.java
+++ com/dsAlgo2023/linkedList/LinkedList.java
@@ -33,15 +33,40 @@ public class LinkedList<T> {
         size++;
     }
 
-    public int size(){
+    public int size() {
         return this.size;
     }
 
-    public void traverse(){
+    public void traverse() {
         Node node = head;
-        while (node!=null){
+        while (node != null) {
             System.out.println(node.data);
-            node=node.next;
+            node = node.next;
         }
+    }
+
+    public void delete(int pos) {
+        if (pos < size() && pos >= 0) {
+            Node node = head;
+            delete(node, pos);
+        } else {
+            throw new IllegalArgumentException("Invalid postion: " + pos);
+        }
+    }
+
+    private void delete(Node node, int pos) {
+        if (pos == 0) {
+            Node deleteingNode = head;
+            head = head.next;
+            deleteingNode.next=null;
+            return;
+        }
+        if (pos == 1) {
+            Node deleteingNode = node.next;
+            node.next = node.next.next;
+            deleteingNode.next= null;
+            return;
+        }
+        delete(node.next, --pos);
     }
 }
