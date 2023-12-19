2149e8f5a56e4a1dfdfadb11f56a20d3af2e6dd8
ishwar chaiser <ishwarchaiser@gmail.com>
2023-12-19 15:56:25 +0530

    java8: Predicates joining

--- com/dsAlgo2023/java8/MyPredicates.java
+++ com/dsAlgo2023/java8/MyPredicates.java
@@ -4,9 +4,14 @@ import java.util.function.Predicate;
 
 public class MyPredicates {
     public static void main(String[] args) {
+        Predicate<Integer> greaterthan18 = i-> i>18;
+        Predicate<Integer> lessthan60 = i-> i<60;
         myFunc(i -> i>18,20);
-        myFunc(i -> i>18,10);
-        myFunc(i -> i>18,18);
+        myFunc(greaterthan18,10);
+        myFunc(lessthan60,65);
+        myFunc(greaterthan18.and(lessthan60),45);
+        myFunc(greaterthan18.and(lessthan60),100);
+
     }
 
     public static void myFunc(Predicate<Integer> predicates,int age){
